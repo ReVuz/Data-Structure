f31bbea036e93126e181cf970ff416afab8f5306
ReVuz <revathymr916@gmail.com>
2022-08-20 22:47:11 +0530

    :art: Error fixed

--- IADAEFAA.cpp
+++ IADAEFAA.cpp
@@ -1,39 +1,49 @@
 #include<iostream>
 using namespace std;
 class IADEFAA {
+	int size;
+	int* data = new int[size];
 public:
-	void traverse(int data[], int size) {
-		cout << "\nArray Traversal : ";
+	IADEFAA() {
+		cout << "Enter the size of an array : ";
+		cin >> size;
+		cout << "Enter the array elements : ";
 		for (int i = 0; i < size; i++) {
-			cout << data[i]<<" ";
+			cin >> data[i];
 		}
 	}
-	int* insert(int data[], int *size,int item,int pos) {
-		*size += 1;
-		cout << "UPDATED ARRAY SIZE : " << *size<< endl;
-		for (int i = *size; i >= pos; i--) {				//n-pos
+	void traverse() {
+		cout << "\nTRAVERSING AN ARRAY : \n";
+		for (int i = 0; i < size; i++) {
+			cout << data[i] <<" ";
+		}
+	}
+	int* insert(int item, int pos) {
+		size += 1;
+		cout << "\nUPDATED ARRAY SIZE : " << size << endl;
+		for (int i = size; i >= pos; i--) {				//n-pos
 			data[i] = data[i - 1];
 		}
-		data[pos - 1] =  item;
-		cout << "Updated array : \n" << endl;
-		for (int i = 0; i < *size; i++) {
-			cout << data[i] <<" ";
+		data[pos - 1] = item;
+		cout << "\nUPDATED ARRAY : " << endl;
+		for (int i = 0; i < size; i++) {
+			cout << data[i] << " ";
 		}
 		return data;
 	}
-	int* del(int data[], int *size, int item) {
+	int* del(int item) {
 		int temp;
-		for (int i = 0; i < *size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (data[i] == item) {
-				temp = data[i+1];
+				temp = data[i + 1];
 				data[i + 1] = data[i];
 				data[i] = temp;
 			}
 		}
-		*size -= 1;
-		cout << "UPDATED ARRAY SIZE : " << *size << endl;
-		cout << "Updated array : ";
-		for (int i = 0; i < *size; i++) {
+		size -= 1;
+		cout << "\nUPDATED ARRAY SIZE : " << size << endl;
+		cout << "\nUPDATED ARRAY : ";
+		for (int i = 0; i < size; i++) {
 			cout << data[i] << " ";
 		}
 		return data;
@@ -41,26 +51,20 @@ public:
 };
 int main() {
 	IADEFAA op;
-	cout << "Enter the size of an array : ";
-	int size, ans = 0;
+	int ans = 0;
 	int option;
-	cin >> size;
-	cout << "Enter the array elements : ";
-	int* data = new int[size];
-	for (int i = 0; i < size; i++) {
-		cin >> data[i];
-	}
 	do {
 		cout << "\nArray Operations : "
 			"\n1. Traverse an array "
 			"\n2. Insert an element "
 			"\n3. Delete an element "
+			"\n4. Exit "
 			"\nChoose any option from above : ";
 		cin >> option;
 		switch (option)
 		{
 		case 1:
-			op.traverse(data, size);
+			op.traverse();
 			break;
 		case 2:
 			cout << "Enter the element to be inserted : ";
@@ -69,14 +73,15 @@ int main() {
 			cout << "Enter the position of element to be inserted : ";
 			int pos;
 			cin >> pos;
-			op.insert(data, &size, item, pos);
+			op.insert(item, pos);
 			break;
-		case 3 :
+		case 3:
 			cout << "Enter the element to be deleted : ";
 			cin >> item;
-			op.del(data ,&size ,item);
+			op.del(item);
 			break;
-
+		case 4 :
+			break;
 		default:
 			break;
 		}
@@ -84,5 +89,6 @@ int main() {
 		cout << "\nDo you want to continue ?(1/0) : ";
 		cin >> ans;
 	} while (ans);
+	cout << "Thank you!" << endl;
 
 }
