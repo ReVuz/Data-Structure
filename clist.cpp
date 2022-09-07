51dbd8e756f738ce93629db0aadc13321039c2af
ReVuz <revathymr916@gmail.com>
2022-09-07 21:49:12 +0530

    :stethoscope: circular header list updated

--- clist.cpp
+++ clist.cpp
@@ -5,81 +5,123 @@ public:
 	int data;
 	node* next;
 };
-node* insert(node* end, int item) {
+void insert(node** head, int item) {
 	node* temp = new node();
 	temp->data = item;
-	if (end == NULL) {
-		end = temp;
-		end->next = end;
-		return end;
-	}
-	else {
-		temp->next = end->next;
-		end->next = temp;
-		end = temp;
-		return end;
-	}
-}
-node* del(node* end, int item) {
-	node* temp = end->next;
-	node* temp1 = end->next;
-	if (end == NULL) {
-		cout << "List is already empty" << endl;
-	}
-	else if (end->data == item && end->next == NULL) {
-		delete end;
-		end = NULL;
-		return end;
-	}
-	else if(end->data == item){
-		while(end->next!=end){
-			end = end->next;
+	temp->next = *head;
+	if (*head != NULL) {
+		node* temp1 = *head;
+		while (temp1->next != *head) {
+			temp1 = temp1->next;
 		}
-		end->next = temp1->next;
-		delete temp;
-	}
-	else if (end->next->data == item) {
-		temp = end->next;
-		end->next = temp->next;
-		delete temp;
-		return end;
+		temp1->next = temp;
 	}
 	else {
-		cout << "No item found" << endl;
+		temp->next = temp;
+		*head = temp;
 	}
-	return end;
 }
-void print(node* end) {
-	node* temp;
-	if (end == NULL) {
+node* del(node* head, int item) {
+	int flag = 0;
+	//case 1 : no node
+	if (head == NULL) {
+		cout << "List is Empty" << endl;
+		return head;
+		flag = 1;
+	}
+	//case 2 : only one node
+	else if (head->next == head) {
+		if (head->data == item) {
+			head = NULL;
+			delete head;
+			cout << "Element deleted" << endl;
+			flag = 1;
+		}return head;
+	}
+	//case 3 : more than one node
+	else {
+		node* temp = head;
+		node* ptr;
+		//case  3.1 : head has to be deleted
+		if(head->data == item){
+			while (temp->next != head) {
+				temp = temp->next;
+			}
+			temp->next = head->next;
+			delete head;
+			head = temp->next;
+			return head;
+		}
+		else {
+			while (temp->next != head && temp->next->data != item) {
+				temp = temp->next;
+			}
+			//case 4 : if element is found somewhere
+			//from the list other than head
+			if (temp->next->data == item) {
+				ptr = temp->next;
+				temp->next = ptr->next;
+				delete ptr;
+				return head;
+			}
+		}flag = 1;
+
+
+	}
+	if (flag == 0) {
+		cout << "Element not found" << endl;
+	}return head;
+}
+void print(node* head){
+	cout << "List : ";
+	if (head == NULL) {
 		cout << "List is Empty" << endl;
 	}
-	//Pointing to first node
-	temp = end->next;
-	cout << "List : ";
-	do {
-		cout << temp->data << " ";
-		temp = temp->next;
-	} while (temp != end->next);
-
+	else {
+		node* temp = head;
+		/*while (temp->next != head) {	
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << temp->data << " ";*/
+		do {
+			cout << temp->data <<" ";
+			temp = temp->next;
+		} while (temp != head);
+	}
 }
 int main() {
-	node* end = NULL;
-	int ans = 0;
+	node* head = NULL;
+	int ans = 0, item;
 	do {
-		cout << "Enter the element to insert : ";
-		int item;
-		cin >> item;
-		end = insert(end, item);
-		print(end);
-		cout << "Enter the element to delete : ";
-		cin >> item;
-		end = del(end, item);
-		print(end);
-		cout << "\nDo you want to start over?(1/0) : ";
+		cout << "\nCircular Header List Operations : "
+			"\n1. Insert Node \n2. Delete Node \n0. Exit"
+			"\nChoose Your Option : ";
+		int option;
+		cin >> option;
+		switch (option) {
+		case 1:
+			cout << "Enter number of elements to insert : ";
+			int num;
+			cin >> num;
+			cout << "Enter the elements : ";
+			for (int i = 0; i < num; i++) {
+				cin >> item;
+				insert(&head, item);
+				//cout << "Element added" << endl;
+			}
+			break;
+		case 2:
+			cout << "Enter the element to delete : ";
+			cin >> item;
+			head = del(head, item);
+			break;
+		default:
+			break;
+		}
+		print(head);
+		cout << "\nDo you want to continue ? (1/0) : ";
 		cin >> ans;
 	} while (ans);
-	cout << "Thank you" << endl;
+	cout << "Thank You" << endl;
 }
-
-
